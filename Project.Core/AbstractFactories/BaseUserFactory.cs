8bf926e0d305525b6d224d89a8ef3522a0a78534
Carmen Le Hanie <carmenlehanie@gmail.com>
2021-07-27 19:32:31 +0200

    Factory Pattern

---
+++ Project.Core/AbstractFactories/BaseUserFactory.cs
@@ -0,0 +1,28 @@
+﻿using Project.Core.Interfaces;
+using Project.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Core.AbstractFactories
+{
+    public abstract class BaseUserFactory
+    {
+        protected User _user;
+
+        public BaseUserFactory(User user)
+        {
+            _user = user;
+        }
+
+        public User ApplyUUID()
+        {
+
+            IUser manager = this.Create();
+            _user.UUID = manager.getUUID();
+            return _user;
+        }
+
+        public abstract IUser Create();
+    }
+}
