f3dd4d314fb7629157884e045802e2e5392f0a77
Carmen Le Hanie <carmenlehanie@gmail.com>
2021-07-27 19:33:28 +0200

    Recruiter Factory

---
+++ Project.Core/ConcreteFactories/RecruiterFactory.cs
@@ -0,0 +1,26 @@
+﻿using Project.Core.AbstractFactories;
+using Project.Core.ConcreteClasses;
+using Project.Core.Interfaces;
+using Project.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Core.ConcreteFactories
+{
+    public class RecruiterFactory : BaseUserFactory
+    {
+        public RecruiterFactory(User user) : base(user)
+        {
+        }
+
+        public override IUser Create()
+        {
+            ConcreteRecruiter recruiter = new ConcreteRecruiter();
+
+            _user.UUID = recruiter.getUUID();
+
+            return recruiter;
+        }
+    }
+}
