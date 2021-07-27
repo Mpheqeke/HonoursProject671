aa8ed971a753d01d103fb4888e2a4d98ed3a7bf3
Carmen Le Hanie <carmenlehanie@gmail.com>
2021-07-27 19:33:07 +0200

    Concrete Class for Recruiter

---
+++ Project.Core/ConcreteClasses/ConcreteRecruiter.cs
@@ -0,0 +1,21 @@
+﻿using Project.Core.Interfaces;
+using Project.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Core.ConcreteClasses
+{
+    public class ConcreteRecruiter : User, IUser
+    {
+        public string getUUID()
+        {
+            Guid g = Guid.NewGuid();
+            string guid = Guid.NewGuid().ToString();
+
+            UUID = guid;
+
+            return UUID;
+        }
+    }
+}
