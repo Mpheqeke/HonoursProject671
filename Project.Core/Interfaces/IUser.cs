a9c70a8206192454565b8b71660d652093405bcf
Carmen Le Hanie <carmenlehanie@gmail.com>
2021-07-27 19:33:40 +0200

    Facotry User Interface

---
+++ Project.Core/Interfaces/IUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Core.Interfaces
+{
+    public interface IUser
+    {
+        string getUUID();
+    }
+}
