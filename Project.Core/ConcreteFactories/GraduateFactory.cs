6721767b202b1f950960b4a39fb09d5b70dd569e
Carmen Le Hanie <carmenlehanie@gmail.com>
2021-07-27 19:33:18 +0200

    Graduate Factory

---
+++ Project.Core/ConcreteFactories/GraduateFactory.cs
@@ -0,0 +1,26 @@
+﻿using Project.Core.AbstractFactories;
+using Project.Core.ConcreteClasses;
+using Project.Core.Interfaces;
+using Project.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Core.ConcreteFactories
+{
+    public class GraduateFactory : BaseUserFactory
+    {
+        public GraduateFactory(User user) : base(user)
+        {
+        }
+
+        public override IUser Create()
+        {
+            ConcreteGraduate grad = new ConcreteGraduate();
+
+            _user.UUID = grad.getUUID();
+
+            return grad;
+        }
+    }
+}
