d6311e2cfc6e03ad0eb88c676aeff1891e7b3beb
Carmen Le Hanie <carmenlehanie@gmail.com>
2021-07-27 19:35:23 +0200

    Abstract factory for Users, used in Base factory

---
+++ Project.Core/AbstractFactories/AbstractUserFactory.cs
@@ -0,0 +1,27 @@
+﻿using Project.Core.ConcreteFactories;
+using Project.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Core.AbstractFactories
+{
+    public class AbstractUserFactory
+    {
+        public BaseUserFactory CreateFactory(User user)
+        {
+            BaseUserFactory returnVal = null;
+
+            if (user.RoleId == 1)
+            {
+                returnVal = new GraduateFactory(user);
+            }
+            else if (user.RoleId == 2)
+            {
+                returnVal = new RecruiterFactory(user);
+            }
+
+            return returnVal;
+        }
+    }
+}
